6bd265d0dec2e54beb60caf837c48657a3dfc399
Danilo Costa <danilo.costa@olist.com>
2021-09-30 09:08:40 -0300

    env(localstack.env): file has been added.

---
+++ s3_wrapper.py
@@ -0,0 +1,59 @@
+import os
+import boto3
+import json
+
+from botocore.exceptions import ClientError
+
+
+def __init_s3():
+    return boto3.resource(
+        "s3",
+        region_name="us-east-1",
+        aws_access_key_id="mock_access_key",
+        aws_secret_access_key="mock_secret_key",
+        endpoint_url="http://localhost:4566",
+    )
+
+
+def create_s3():
+    s3 = __init_s3()
+    return s3.create_bucket(Bucket="teste")
+
+
+def send_object(object_name=None):
+    create_s3()
+    file_name = "Dockerfile"
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client(
+        "s3",
+        region_name="us-east-1",
+        aws_access_key_id="mock_access_key",
+        aws_secret_access_key="mock_secret_key",
+        endpoint_url="http://localhost:4566",
+    )
+    try:
+        response = s3_client.upload_file(file_name, "teste", object_name)
+    except ClientError as e:
+        print(e)
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    send_object()
+
+
+# def return_all():
+#     queue = __get_queue_by_name("teste")
+#     messages = queue.receive_messages()
+#     result = []
+#     while len(messages) > 0:
+#         message = messages[0]
+#         json_returned = json.loads(message.body)
+#         result.append(json_returned)
+#         message.delete()
+#         messages = queue.receive_messages()
+#     return result
